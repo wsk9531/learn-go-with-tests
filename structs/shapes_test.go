8c36468d78232c1b0fa0054c4af517a1b1aa9ef5
ben-wsk9531 <wsk9531@autuni.ac.nz>
2022-06-29 19:46:35 +1200

    refactored to use table-based tests of interface properties

--- structs/shapes_test.go
+++ structs/shapes_test.go
@@ -14,23 +14,21 @@ func TestPerimeter(t *testing.T) {
 
 func TestArea(t *testing.T) {
 
-	checkArea := func (t testing.TB, shape Shape, want float64) {
-		t.Helper()
-		got := shape.Area()
-
-		if got != want {
-			t.Errorf("got %.2g, want %.2g", got, want)
-		}
+	areaTests := []struct {
+		name string
+		shape Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12.0, Height: 6.0}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10.0}, hasArea: 314.1592653589793}, 
+		{name: "Triangle", shape: Triangle{Base: 12.0, Height: 6.0}, hasArea: 36.0},
 	}
-
-	t.Run("Rectangles", func (t *testing.T) {
-		rectangle := Rectangle{10.0, 10.0}
-		checkArea(t, rectangle, 100.0)
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := tt.shape.Area()
+			if got != tt.hasArea {
+				t.Errorf("%#v got %g want %g", tt.shape, got, tt.hasArea)
+			}
 	})
-
-	t.Run("Circles", func (t *testing.T) {
-
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	}
 }
